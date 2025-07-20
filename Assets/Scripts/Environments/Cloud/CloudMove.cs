16c5e7e0ebd620fd9fcb6b133827400bc83f8ef8
thanhdat2011 <chauthanhdat2000@gmail.com>
2025-07-20 12:23:22 +0700

    prefab

---
+++ Assets/Scripts/Environments/Cloud/CloudMove.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+
+public class CloudMove : MonoBehaviour
+{
+
+    [Header("Speed")]
+    public float minSpeed = 0.1f;
+    public float maxSpeed = 0.6f;
+
+    [HideInInspector] public float moveSpeed;
+
+    private float width;
+    private Camera mainCam;
+    private float screenLeft, screenRight;
+
+    void Start()
+    {
+        mainCam = Camera.main;
+        width = GetComponent<SpriteRenderer>().bounds.size.x;
+        UpdateScreenBounds();
+        moveSpeed = Random.Range(minSpeed, maxSpeed);
+    }
+
+    void Update()
+    {
+        transform.Translate(Vector3.left * moveSpeed * Time.deltaTime);
+
+        if (transform.position.x + width < screenLeft)
+        {
+            float newX = screenRight + width;
+            float newY = transform.position.y;
+
+            // reposition và random lại tốc độ
+            transform.position = new Vector3(newX, newY, transform.position.z);
+            moveSpeed = Random.Range(minSpeed, maxSpeed);
+        }
+    }
+
+    void UpdateScreenBounds()
+    {
+        float height = 2f * mainCam.orthographicSize;
+        float width = height * mainCam.aspect;
+
+        screenLeft = mainCam.transform.position.x - width / 2;
+        screenRight = mainCam.transform.position.x + width / 2;
+    }
+
+}
---
+++ Assets/Scripts/Environments/Trees/TreeGroupSpawner.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+
+public class TreeGroupSpawner : MonoBehaviour
+{
+
+    public GameObject[] cloudPrefabs;
+    public int cloudCount = 10;
+
+    public Vector2 xRange = new Vector2(-10f, 10f);
+    public Vector2 speedRange = new Vector2(0.1f, 0.6f);
+
+    private float minY;
+    private float maxY;
+
+    void Start()
+    {
+        CalculateYBoundsFromCamera();
+
+        for (int i = 0; i < cloudCount; i++)
+        {
+            GameObject prefab = cloudPrefabs[Random.Range(0, cloudPrefabs.Length)];
+
+            float x = Random.Range(xRange.x, xRange.y);
+            float y = Random.Range(minY, maxY);
+            Vector3 spawnPos = new Vector3(x, y, 0f);
+
+            GameObject cloud = Instantiate(prefab, spawnPos, Quaternion.identity, transform);
+
+            CloudMove moveScript = cloud.GetComponent<CloudMove>();
+            if (moveScript != null)
+            {
+                moveScript.minSpeed = speedRange.x;
+                moveScript.maxSpeed = speedRange.y;
+                moveScript.moveSpeed = Random.Range(speedRange.x, speedRange.y);
+            }
+        }
+    }
+
+    void CalculateYBoundsFromCamera()
+    {
+        Camera cam = Camera.main;
+        float camHeight = 2f * cam.orthographicSize;
+        float camBottom = cam.transform.position.y - camHeight / 2;
+        float camTop = cam.transform.position.y + camHeight / 2;
+
+        minY = camBottom + 0.5f;
+        maxY = camTop - 0.5f;
+    }
+
+}
